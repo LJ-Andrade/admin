adc26821756c5e2e5e56efc64ffd27d4536944d3
javzero <javzero@hotmail.com>
2016-08-14 21:25:41 -0300

    Working on Products > New

--- files/modules/product/new.php
+++ files/modules/product/new.php
@@ -1,27 +1,191 @@
 <?php
-  include('../../includes/inc.main.php');
-  $Menu   = new Menu();
-  $Head->setTitle("Blank");
-  $Head->setSubTitle("Page");
-  $Head->setHead();
-  include('../../includes/inc.top.php');
- ?>
+    include("../../includes/inc.main.php");
+    //$Head->setTitle("Nuevo Usuario");
+    $Menu   = new Menu();
+    $Group  = new GroupData();
+    $Head->setTitle($Menu->GetLinkTitle());
+    $Head->setStyle('../../../vendors/bootstrap-switch/bootstrap-switch.css'); // Switch On Off
+    $Head->setHead();
+    include('../../includes/inc.top.php');
 
- <div class="box box-success">
-   <div class="box-header with-border">
-     <h3 class="box-title">Prod</h3>
-     <div class="box-tools pull-right">
-       <!-- Buttons, labels, and many other things can be placed here! -->
-     </div><!-- /.box-tools -->
-   </div><!-- /.box-header -->
-   <div class="box-body">
-     The body of the box
-   </div><!-- /.box-body -->
-   <div class="box-footer">
-     The footer of the box
-   </div><!-- box-footer -->
- </div><!-- /.box -->
 
-<?php
-  include('../../includes/inc.bottom.php');
 ?>
+  <?php echo insertElement("hidden","action",'insert'); ?>
+  <?php echo insertElement("hidden","menues",""); ?>
+  <?php echo insertElement("hidden","groups",""); ?>
+  <?php echo insertElement("hidden","newimage",$Admin->DefaultImg); ?>
+
+   <div class="box"> <!--box-success-->
+    <div class="box-header with-border">
+      <h3 class="box-title">Complete el formulario</h3>
+    </div><!-- /.box-header -->
+    <div class="box-body">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="flex-allCenter innerContainer">
+            <div class="mw100">
+              <h4 class="subTitleB"><i class="fa fa-tag"></i> Datos Principales</h4>
+              <div class="form-group">
+                <input type="email" class="form-control" placeholder="Nombre del Producto">
+              </div>
+              <div class="form-group">
+                <input type="email" class="form-control" placeholder="C&oacute;digo">
+              </div>
+              <div class="form-group">
+                <input type="email" class="form-control" placeholder="Composici&oacute;n">
+              </div>
+              <div class="form-group">
+                <input type="email" class="form-control" placeholder="Precio">
+              </div>
+            <!-- Description -->
+            <div class="form-group2">
+              <form>
+
+              <textarea id="description" name="description" class="text-center" placeholder="Descripción" rows="4" maxlength="150"></textarea>
+              <div class="indicator-wrapper">
+                <p>Caracteres restantes</p>
+                <div class="indicator"><span class="current-length">150</span></div>
+              </div>
+              </form>
+
+            </div>
+            <!-- Description -->
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="flex-allCenter innerContainer">
+            <div class="">
+              <h4 class="subTitleB"><i class="fa fa-tag"></i> Talles</h4>
+              <div class="form-group boxed-horiz-list">
+                <ul>
+                  <li><input id="sizeXS" class="CheckBox TreeCheckbox checkbox-custom" type="checkbox"><label class="checkbox-custom-label" for="sizeXS"><span>XS</span></li>
+                  <li><input id="sizeS" class="CheckBox TreeCheckbox checkbox-custom" type="checkbox"><label class="checkbox-custom-label" for="sizeS"><span>S</span></li>
+                  <li><input id="sizeM" class="CheckBox TreeCheckbox checkbox-custom" type="checkbox"><label class="checkbox-custom-label" for="sizeM"><span>M</span></li>
+                  <li><input id="sizeL" class="CheckBox TreeCheckbox checkbox-custom" type="checkbox"><label class="checkbox-custom-label" for="sizeL"><span>L</span></li>
+                  <li><input id="sizeXL" class="CheckBox TreeCheckbox checkbox-custom" type="checkbox"><label class="checkbox-custom-label" for="sizeXL"><span>XL</span></li>
+                  <li><input id="sizeXXL" class="CheckBox TreeCheckbox checkbox-custom" type="checkbox"><label class="checkbox-custom-label" for="sizeXXL"><span>XXL</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="flex-allCenter innerContainer">
+            <div class="">
+              <h4 class="subTitleB"><i class="fa fa-tag"></i> Estado Inicial</h4>
+              <div class="switchCheckbox">
+                <input type="checkbox" name="switchCheckbox" data-on-text="Activo" data-off-text="Inactivo" data-label-width="auto" checked>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+
+        </div>
+      </div><!-- row -->
+
+
+
+      <!-- IMAGES -->
+      <!-- Actual Image -->
+      <div class="row imagesMain">
+        <div class="col-lg-3 col-md-12 col-sm-6 col-xs-12">
+          <div class="imagesContainer">
+            <h4 class="subTitleB"><i class="fa fa-picture-o"></i> Im&aacute;gen Actual</h4>
+            <div class="flex-allCenter imgSelector">
+              <div class="imgSelectorInner">
+                <img src="<?php echo $Admin->DefaultImg ?>" class="img-responsive MainImg">
+                <?php echo insertElement('file','image','','Hidden'); ?>
+                <div class="imgSelectorContent">
+                  <div id="SelectImg">
+                    <i class="fa fa-upload"></i><br>
+                   <p>Cargar Nueva Im&aacute;gen</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="text-bottom">
+              <p><i class="fa fa-upload" aria-hidden="true"></i>
+              Haga Click sobre la im&aacute;gen </br> para cargar una desde su dispositivo</p>
+            </div>
+          </div>
+        </div><!-- /Actual Image -->
+        <!-- Generic Images -->
+        <div class="col-lg-5 col-md-12 col-sm-6 col-xs-12">
+          <div class="imagesContainer">
+            <h4 class="subTitleB"><i class="fa fa-picture-o"></i> Im&aacute;genes Gen&eacute;ricas</h4>
+            <div class="smallThumbsList flex-justify-center">
+              <ul>
+                <?php
+                  foreach($Admin->DefaultImages() as $Image)
+                  {
+                    echo '<li><img src="'.$Image.'" class="ImgSelecteable"></li>';
+                  }
+                ?>
+              </ul>
+            </div>
+             <div class="text-bottom">
+               <p><i class="fa fa-check" aria-hidden="true"></i>
+               Seleccione una im&aacute;gen para utilizarla</p>
+            </div>
+          </div>
+        </div><!-- /Generic Images -->
+        <!-- Recent Images -->
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+          <div class="imagesContainer">
+            <h4 class="subTitleB"><i class="fa fa-picture-o"></i> Im&aacute;genes usadas anteriormente</h4>
+            <div class="smallThumbsList flex-justify-center">
+              <ul id="UserImages">
+                <?php
+                  foreach($Admin->UserImages() as $Image)
+                  {
+                    echo '<li><img src="'.$Image.'" class="ImgSelecteable"></li>';
+                  }
+                ?>
+              </ul>
+            </div>
+             <div class="text-bottom">
+               <p><i class="fa fa-check" aria-hidden="true"></i>
+              Seleccione una im&aacute;gen para utilizarla</p>
+            </div>
+          </div>
+        </div><!-- /Recent Images -->
+      </div><!-- IMAGES -->
+    </div><!-- /.box-body -->
+    <div class="box-footer btnRightMobCent">
+      <button type="button" class="btn btn-success btnGreen" id="BtnCreate"><i class="fa fa-plus"></i> Crear Nuevo Usuario</button>
+      <button type="button" class="btn btn-success btnBlue" id="BtnCreateNext"><i class="fa fa-plus"></i> Crear y Agregar Otro</button>
+      <button type="button" class="btn btn-danger btnRed" id="BtnCancel"><i class="fa fa-times"></i> Cancelar</button>
+    </div><!-- box-footer -->
+  </div><!-- /.box -->
+  <!-- Help Modal Trigger -->
+  <button type="button" class="btn btn-success btnGrey" data-toggle="modal" data-target="#helpModal" ><i class="fa fa-question-circle"></i> Ayuda</button>
+  <!-- Help Modal Trigger -->
+  <!-- //// HELP MODAL //// -->
+  <div id="helpModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Ayuda para el usuario</i></h4>
+        </div>
+        <div class="modal-body">
+          <p>
+            Ayuda sobre categorias
+          </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" name="button" class="btn btn-success btnBlue" data-dismiss="modal">Comprendido</button><br>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Help Modal -->
+<?php
+$Foot->setScript('../../../vendors/bootstrap-switch/script.bootstrap-switch.min.js');
+include('../../includes/inc.bottom.php');
+?>
+<script type="text/javascript">
+////////////////// Bootstrap Switch ////////////
+$("[name='switchCheckbox']").bootstrapSwitch();
+</script>
--- files/modules/product/script.js
+++ files/modules/product/script.js
@@ -215,3 +215,23 @@ $(document).ready(function() {
   // By default, activate simplecolorpicker plugin on HTML selects
   $('#init').trigger('click');
 });
+
+
+//////////////////// Character Counter ///////////////////////////
+$('input, textarea').keyup(function() {
+  var max = $(this).attr('maxLength');
+  var curr = this.value.length;
+  var percent = (curr/max) * 100;
+  var indicator = $(this).parent().children('.indicator-wrapper').children('.indicator').first();
+
+  // Shows characters left
+  indicator.children('.current-length').html(max - curr);
+
+  // Change colors
+  if (percent > 30 && percent <= 50) { indicator.attr('class', 'indicator low'); }
+  else if (percent > 50 && percent <= 70) { indicator.attr('class', 'indicator med'); }
+  else if (percent > 70 && percent < 100) { indicator.attr('class', 'indicator high'); }
+  else if (percent == 100) { indicator.attr('class', 'indicator full'); }
+  else { indicator.attr('class', 'indicator empty'); }
+  indicator.width(percent + '%');
+});
